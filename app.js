8b74fece5a7c30f9c281bc3deed342aa4f27486a
Kevin Thomas <kevin_thomas819@yahoo.com>
2022-02-10 07:12:52 -0500

    install/ use inquirer to ask questions

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const inquirer = require("inquirer");
+/*
 const fs = require("fs");
 const generatePage = require("./src/page-template.js");
 
-// get input from command line then put into an array
-const profileDataArgs = process.argv.slice(2);
-
-//assignment destructuring to assign elements of an array to variable names in a single epression
-const [name, github] = profileDataArgs;
+const pageHtml = generatePage(name, github);
 
 //write template into file system as html doc to generate webpage, check for errors and print message 
 fs.writeFile("index.html", generatePage(name, github), err => {
     if(err) throw new Error(err);
     console.log("Portfolio complete! Check out index.html to see the output!");
 });
+*/
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
