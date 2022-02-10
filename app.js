a06e508e65da30ef3336ae4a063f1aad290bd1c4
Kevin Thomas <kevin_thomas819@yahoo.com>
2022-02-10 02:15:25 -0500

    generate html file to display webpage

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
 // get input from command line then put into an array
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const profileDataArgs = process.argv.slice(2);
 
-//print each element of new array 
-const printProfileData = profileDataArr => 
-{
-    //array method to iterate through array 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+//assignment destructuring to assign elements of an array to variable names in a single epression
+const [name, github] = profileDataArgs;
 
-
-printProfileData(profileDataArgs);
+//write template into file system as html doc to generate webpage, check for errors and print message 
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if(err) throw new Error(err);
+    console.log("Portfolio complete! Check out index.html to see the output!");
+});
